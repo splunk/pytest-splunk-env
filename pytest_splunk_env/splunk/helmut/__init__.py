368aee5500948694ab8c151fc3329b0fe12e8409
rfaircloth-splunk <rfaircloth@splunk.com>
2020-10-21 10:32:36 -0400

    Update __init__.py

--- pytest_splunk_env/splunk/helmut/__init__.py
+++ pytest_splunk_env/splunk/helmut/__init__.py
@@ -1,9 +1,3 @@
 # SPDX-FileCopyrightText: 2020 Splunk Inc.
 #
 # SPDX-License-Identifier: Apache-2.0
-
-"""
-@author: Nicklas Ansman-Giertz
-@contact: U{ngiertz@splunk.com<mailto:ngiertz@splunk.com>}
-@since: 2011-11-23
-"""
